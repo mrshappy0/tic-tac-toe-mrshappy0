02a1afe100c93f505d1709c21cec6b159e400949
mrshappy0 <ajs11300@gmail.com>
2019-06-24 11:18:03 -0400

    Added win detection

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let numberCount = 0;
-function TurnCounter () {
+function TurnCounter() {
 	let turnCountArea = document.getElementById("Turn Count");
 	numberCount++;
 	turnCountArea.textContent = "Turn Count: " + numberCount + ".";
@@ -20,26 +20,81 @@ function turnCheck(turn) {
 	}
 }
 
-function blockEvent (turn, cell, xOrO){
+function blockEvent(turn, cell, xOrO) {
 	if (cell.innerHTML === "") {
 		cell.innerHTML = turn;
 		statusArea.textContent = "Player " + xOrO + " Turn";
 		TurnCounter();
 	} else {
-		alert ("Try an empty cell!");
+		alert("Try an empty cell!");
 	}
 }
 
 let cellArray = document.querySelectorAll(".cells");
 for (const cell of cellArray) {
 	cell.addEventListener("click", () => {
-
 		if (turnCheck(statusArea.textContent) === true) {
 			blockEvent("X", cell, "O");
+			checkWin();
 		} else {
 			blockEvent("O", cell, "X");
+			checkWin();
 		}
 	});
 }
+
+function checkWin() {
+	if (
+		document.getElementById("cell-0").textContent === "X" &&
+		document.getElementById("cell-1").textContent === "X" &&
+		document.getElementById("cell-2").textContent === "X"
+	) {
+		alert("Win");
+	} else if (
+		document.getElementById("cell-3").textContent === "X" &&
+		document.getElementById("cell-4").textContent === "X" &&
+		document.getElementById("cell-5").textContent === "X"
+	) {
+		alert("Win");
+	} else if (
+		document.getElementById("cell-6").textContent === "X" &&
+		document.getElementById("cell-7").textContent === "X" &&
+		document.getElementById("cell-8").textContent === "X"
+	) {
+		alert("Win");
+	} else if (
+		document.getElementById("cell-0").textContent === "O" &&
+		document.getElementById("cell-3").textContent === "O" &&
+		document.getElementById("cell-6").textContent === "O"
+	) {
+		alert("Win");
+	} else if (
+		document.getElementById("cell-1").textContent === "O" &&
+		document.getElementById("cell-4").textContent === "O" &&
+		document.getElementById("cell-7").textContent === "O"
+	) {
+		alert("Win");
+	} else if (
+		document.getElementById("cell-2").textContent === "O" &&
+		document.getElementById("cell-5").textContent === "O" &&
+		document.getElementById("cell-8").textContent === "O"
+	) {
+		alert("Win");
+	} else if (
+		document.getElementById("cell-0").textContent === "O" &&
+		document.getElementById("cell-4").textContent === "O" &&
+		document.getElementById("cell-8").textContent === "O"
+	) {
+		alert("Win");
+	} else if (
+		document.getElementById("cell-2").textContent === "O" &&
+		document.getElementById("cell-4").textContent === "O" &&
+		document.getElementById("cell-6").textContent === "O"
+	) {
+	alert("Win");
+	}
+}
+
+
 
 
